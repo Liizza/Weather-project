9e52310e8d78d60c393dcd490279fa7b32e798f6
Liizza <95987622+Liizza@users.noreply.github.com>
2022-07-01 01:00:00 +0300

    Improved CSS/JS,added icons

--- src/weather.js
+++ src/weather.js
@@ -1,9 +1,10 @@
-let now = new Date();
+
 let apiKey = "95c346137f1a7a732a951de63cafdb0a";
 let apiWeather = `https://api.openweathermap.org/data/2.5/weather?`;
-let enterCity = document.querySelector("#enterCity");
 
-function showNow(newDate) {
+
+function showTime(newDate) {
+  let time = new Date(newDate);
   let days = [
     "Sunday",
     "Monday",
@@ -13,22 +14,23 @@ function showNow(newDate) {
     "Friday",
     "Saturday"
   ];
-  let currentDay = days[newDate.getDay()];
-  let currentHours = newDate.getHours();
+  let currentDay = days[time.getDay()];
+  let currentHours = time.getHours();
   if (currentHours < 10) {
     currentHours = `0${currentHours}`;
   }
-  let currentMinutes = newDate.getMinutes();
+  let currentMinutes = time.getMinutes();
   if (currentMinutes < 10) {
     currentMinutes = `0${currentMinutes}`;
   }
   let fullTime = `${currentDay} ${currentHours}:${currentMinutes}`;
-  let currentTime = document.querySelector("#currentTime");
-  currentTime.innerHTML = `${fullTime}`;
+  return fullTime;
 }
-function showTemp(response) {
-  let tempCity = Math.round(response.data.main.temp);
-  document.querySelector("#currentTemp").innerHTML = tempCity;
+function showWeather(response) {
+  let choosenCity=response.data.name;
+  document.querySelector("#city").innerHTML=choosenCity;
+  tempCelsius= response.data.main.temp;
+  document.querySelector("#currentTemp").innerHTML = Math.round(tempCelsius);
   let description = response.data.weather[0].main;
   document.querySelector("#description").innerHTML = description;
   let maxTemp = Math.round(response.data.main.temp_max);
@@ -39,47 +41,61 @@ function showTemp(response) {
   document.querySelector("#wind").innerHTML = wind;
   let humidity = response.data.main.humidity;
   document.querySelector("#humidity").innerHTML = humidity;
+  let icon= response.data.weather[0].icon;
+  document.querySelector("#currentIcon").setAttribute("src", `images/icons/${icon}.png`)
+  let date=response.data.dt * 1000;
+  document.querySelector("#currentTime").innerHTML=showTime(date);
 }
-function changeWeather() {
-  let currentCity = document.querySelector("#city").textContent;
-  let apiWeatherCity = `${apiWeather}q=${currentCity}&appid=${apiKey}&&units=metric`;
-  axios.get(apiWeatherCity).then(showTemp);
+function searchCity(city) {
+  let apiWeatherCity = `${apiWeather}q=${city}&appid=${apiKey}&&units=metric`;
+  axios.get(apiWeatherCity).then(showWeather);
 }
-function showCity(event) {
+function showData(event) {
   event.preventDefault();
-  let city = document.querySelector("#city");
-  city.innerHTML = enterCity.value;
-  changeWeather();
+  let enterCity = document.querySelector("#enterCity").value;
+  searchCity(enterCity);
 }
-function currentLocationWeather(response) {
-  showTemp(response);
-  let city = document.querySelector("#city");
-  city.innerHTML = response.data.name;
-}
-function retrievePosition(position) {
+
+
+function searchPosition(position) {
   let lat = position.coords.latitude;
   let lon = position.coords.longitude;
   let apiWeatherPosition = `${apiWeather}lat=${lat}&lon=${lon}&units=metric&appid=${apiKey}`;
-  axios.get(apiWeatherPosition).then(currentLocationWeather);
+  axios.get(apiWeatherPosition).then(showWeather);
 }
-function showLocation() {
-  navigator.geolocation.getCurrentPosition(retrievePosition);
+function showLocation(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(searchPosition);
 }
-showNow(now);
-let changeCity = document.querySelector("form.changeCity");
-changeCity.addEventListener("submit", showCity);
-changeWeather();
+function showF(event) {
+  event.preventDefault();
+  fahrenheit.classList.add("active");
+  celsius.classList.remove("active");
+  let temp=document.querySelector("#currentTemp");
+  let currentTempF=(tempCelsius)*9/5+32;
+  temp.innerHTML =Math.round(currentTempF);
+}
+function showC(event){
+  event.preventDefault();
+  fahrenheit.classList.remove("active");
+  celsius.classList.add("active");
+  let temp=document.querySelector("#currentTemp");
+  temp.innerHTML =Math.round(tempCelsius);
+}
+searchCity("Kyiv");
+let changeCity = document.querySelector("#changeCity");
+changeCity.addEventListener("submit", showData);
+
 let buttonPosition = document.querySelector("#currentLocation");
 buttonPosition.addEventListener("click", showLocation);
 
+let tempCelsius=null;
+
+
 let fahrenheit = document.querySelector("#fahrenheit");
-function showF(event) {
-  event.preventDefault();
-  let currentTemp = document.querySelector("#currentTemp");
-  let currentTempF=(currentTemp.textContent)*9/5+32;
-  currentTemp.innerHTML =Math.round(currentTempF);
-}
 fahrenheit.addEventListener("click", showF);
+let celsius=document.querySelector("#celsius");
+celsius.addEventListener("click",showC)
 
   
   
