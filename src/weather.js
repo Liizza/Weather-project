890f10172f2d09706cea6148ec6a8dc78d83f131
Liizza <95987622+Liizza@users.noreply.github.com>
2022-07-02 03:00:26 +0300

    added forecast

--- src/weather.js
+++ src/weather.js
@@ -26,6 +26,52 @@ function showTime(newDate) {
   let fullTime = `${currentDay} ${currentHours}:${currentMinutes}`;
   return fullTime;
 }
+function formatDay(newDate) {
+  let date = new Date(newDate * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[day];
+}
+function showForecast(response){
+  let forecast = response.data.daily;
+  let forecast5days = document.querySelector("#forecast5days");
+  let forecastData=`<div>`;
+  document.querySelector("#maxTemp").innerHTML=Math.round(forecast[0].temp.max);
+  document.querySelector("#minTemp").innerHTML=Math.round(forecast[0].temp.min);
+  forecast.forEach(function (forecastDay, index) {
+    if(index<6&&index>0){
+      tempMinCelsiusForecast=forecastDay.temp.min;
+      tempMaxCelsiusForecast=forecastDay.temp.max;
+      forecastData=forecastData+`
+      <div class="card mb-3">
+        <div class="card-body">
+          <h5 class="card-title">
+            <div class="row">
+              <div class="col">
+              ${formatDay(forecastDay.dt)}
+              </div>
+              <div class="col">
+                <img src="images/icons/${
+                  forecastDay.weather[0].icon
+                }.png" class="forecastIcon" width="70">
+              </div>
+              <div class="col-6" >
+                H:<span class="maxTempDays">${Math.round(tempMaxCelsiusForecast)}</span>° L:<span class="minTempDays">${Math.round(tempMinCelsiusForecast)}</span>°
+              </div>
+            </div>
+          </h5>
+        </div>
+      </div>`;
+    }
+  });
+  forecastData=forecastData+`</div>`;
+  forecast5days.innerHTML=forecastData; 
+}
+function searchForecast(coordinates) {
+  let apiWeatherForecast = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiWeatherForecast).then(showForecast);
+}
 function showWeather(response) {
   let choosenCity=response.data.name;
   document.querySelector("#city").innerHTML=choosenCity;
@@ -33,10 +79,6 @@ function showWeather(response) {
   document.querySelector("#currentTemp").innerHTML = Math.round(tempCelsius);
   let description = response.data.weather[0].main;
   document.querySelector("#description").innerHTML = description;
-  let maxTemp = Math.round(response.data.main.temp_max);
-  document.querySelector("#maxTemp").innerHTML = maxTemp;
-  let minTemp = Math.round(response.data.main.temp_min);
-  document.querySelector("#minTemp").innerHTML = minTemp;
   let wind = Math.round(response.data.wind.speed);
   document.querySelector("#wind").innerHTML = wind;
   let humidity = response.data.main.humidity;
@@ -45,6 +87,7 @@ function showWeather(response) {
   document.querySelector("#currentIcon").setAttribute("src", `images/icons/${icon}.png`)
   let date=response.data.dt * 1000;
   document.querySelector("#currentTime").innerHTML=showTime(date);
+  searchForecast(response.data.coord);
 }
 function searchCity(city) {
   let apiWeatherCity = `${apiWeather}q=${city}&appid=${apiKey}&&units=metric`;
@@ -74,6 +117,7 @@ function showF(event) {
   let temp=document.querySelector("#currentTemp");
   let currentTempF=(tempCelsius)*9/5+32;
   temp.innerHTML =Math.round(currentTempF);
+  
 }
 function showC(event){
   event.preventDefault();
@@ -90,7 +134,8 @@ let buttonPosition = document.querySelector("#currentLocation");
 buttonPosition.addEventListener("click", showLocation);
 
 let tempCelsius=null;
-
+let tempMinCelsiusForecast=null;
+let tempMaxCelsiusForecast=null;
 
 let fahrenheit = document.querySelector("#fahrenheit");
 fahrenheit.addEventListener("click", showF);
